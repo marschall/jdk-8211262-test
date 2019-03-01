b086ef8006d3a32617da74946411c05aae2dcca3
Philippe Marschall <philippe.marschall@netcetera.com>
2019-03-01 13:16:00 +0100

    updates

--- src/test/java/com/github/marschall/jdk8211262/JDK8211262Tests.java
+++ src/test/java/com/github/marschall/jdk8211262/JDK8211262Tests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Currency;
@@ -20,14 +21,25 @@ class JDK8211262Tests {
   private static final Locale SWITZERLAND = new Locale("de", "CH");
 
   @Test
-  void decimalFormatNoPattern() {
+  void decimalFormat() {
     DecimalFormat format = (DecimalFormat) NumberFormat.getInstance(SWITZERLAND);
     format.applyPattern("#,##0.00");
     
     String formatted = format.format(new BigDecimal("1234567.8"));
     assertEquals("1'234'567.80", formatted);
-    assertEquals('\'', format.getDecimalFormatSymbols().getGroupingSeparator());
-    format.getDecimalFormatSymbols().setGroupingSeparator('\'');
+  }
+  
+  @Test
+  void decimalFormatExplicit() {
+    DecimalFormat format = (DecimalFormat) NumberFormat.getInstance(SWITZERLAND);
+    format.applyPattern("#,##0.00");
+    DecimalFormatSymbols decimalFormatSymbols = format.getDecimalFormatSymbols();
+    decimalFormatSymbols.setGroupingSeparator('\'');
+    // important because getDecimalFormatSymbols returns a clone
+    format.setDecimalFormatSymbols(decimalFormatSymbols);
+    
+    String formatted = format.format(new BigDecimal("1234567.8"));
+    assertEquals("1'234'567.80", formatted);
   }
   
   @Test
@@ -37,6 +49,20 @@ class JDK8211262Tests {
     format.setParseBigDecimal(true);
     
     BigDecimal parsed = (BigDecimal) format.parse("1’234’567.80");
+    assertThat(parsed, comparesEqualTo(new BigDecimal("1234567.8")));
+  }
+  
+  @Test
+  void parseExplicit() throws ParseException {
+    DecimalFormat format = (DecimalFormat) NumberFormat.getInstance(SWITZERLAND);
+    format.applyPattern("#,##0.00");
+    DecimalFormatSymbols decimalFormatSymbols = format.getDecimalFormatSymbols();
+    decimalFormatSymbols.setGroupingSeparator('\'');
+    // important because getDecimalFormatSymbols returns a clone
+    format.setDecimalFormatSymbols(decimalFormatSymbols);
+    format.setParseBigDecimal(true);
+    
+    BigDecimal parsed = (BigDecimal) format.parse("1'234'567.80");
     assertThat(parsed, comparesEqualTo(new BigDecimal("1234567.8")));
   }
   
@@ -63,9 +89,22 @@ class JDK8211262Tests {
   }
   
   @Test
-  void currencyFormatLocal() {
-    Currency currency = Currency.getInstance(SWITZERLAND);
+  void currencyFormat() {
     NumberFormat format = NumberFormat.getCurrencyInstance(SWITZERLAND);
+    format.setCurrency(Currency.getInstance("CHF"));
+    
+    String formatted = format.format(new BigDecimal("1234567.8"));
+    assertEquals("SFr. 1'234'567.80", formatted);
+  }
+  
+  @Test
+  void currencyFormatExplicit() {
+    DecimalFormat format = (DecimalFormat) NumberFormat.getCurrencyInstance(SWITZERLAND);
+    format.setCurrency(Currency.getInstance("CHF"));
+    DecimalFormatSymbols decimalFormatSymbols = format.getDecimalFormatSymbols();
+    decimalFormatSymbols.setGroupingSeparator('\'');
+    // important because getDecimalFormatSymbols returns a clone
+    format.setDecimalFormatSymbols(decimalFormatSymbols);
     
     String formatted = format.format(new BigDecimal("1234567.8"));
     assertEquals("SFr. 1'234'567.80", formatted);
